5f5481e3950a359a1a4715e0e4de4140a82f5ed0
KAROUANI <ykarouani@gmail.com>
2015-07-28 10:52:42 +0100

    Add Comment To this Classes

--- src/com/karouani/cicin/widget/AutocompleteCustomArrayAdapter.java
+++ src/com/karouani/cicin/widget/AutocompleteCustomArrayAdapter.java
@@ -14,7 +14,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
+/*********
+ * 
+ * @author yassinekarouani
+ *
+ */
 public class AutocompleteCustomArrayAdapter extends ArrayAdapter<String> {
 
 	final String TAG = "AutocompleteCustomArrayAdapter.java";
